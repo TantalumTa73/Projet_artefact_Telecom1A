671e43b4ec3f5bfaf003dd1f6546a595b3b3379a
jeffrey <jeffrey.tiger@telecom-paris.fr>
2024-11-08 16:42:37 +0100

    jb

--- code_raspberry/webpage.py
+++ code_raspberry/webpage.py
@@ -101,5 +101,5 @@ if __name__ == '__main__':
 	# run() method of Flask class runs the application 
 	# on the local development server.
 	#app.run()
-	app.run(debug=False,host="0.0.0.0")
+	app.run(debug=True,host="0.0.0.0")
 
