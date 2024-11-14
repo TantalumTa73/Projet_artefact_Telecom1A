7e5bf780afd456cd57f7ab11a027e1fed074358e
ajeannin-24 <alice.jeannin@telecom-paris.fr>
2024-11-14 09:16:54 +0100

    webpage.py,test changer url image (6)

--- code_raspberry/module_camera.py
+++ code_raspberry/module_camera.py
@@ -84,7 +84,7 @@ try :
             
             #convertion en png
             if result is not None:
-                cv2.imwrite("static/image.png", image)
+                cv2.imwrite("image.png", image)
                 print("image capturée")
             else:
                 print("image non capturée")
