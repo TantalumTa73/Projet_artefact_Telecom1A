8decc20b94322ac40bed7a4197c11adf39689491
ajeannin-24 <alice.jeannin@telecom-paris.fr>
2024-11-14 09:32:16 +0100

    webpage.py,test changer url image (12)

--- code_raspberry/module_camera.py
+++ code_raspberry/module_camera.py
@@ -106,6 +106,30 @@ except ModuleNotFoundError:
 except:
     print("\nerreur lors de l'ouverture du module")
 
+import cv2
+cam_port = 0
+
+def save_image():
+        """sauvegarde l'image dans le fichier 'image.png'
+        Return 0 s'il n'y a pas eu d'erreur, 1 sinon"""
+        try:
+            print("is about to try to save image")
+            cam = cv2.VideoCapture(cam_port)
+            print("had tried to save image")
+            result, image = cam.read()
+            
+            #convertion en png
+            if result is not None:
+                print("essaye d'écrire l'image dans image.png")
+                cv2.imwrite("image.png", image)
+                print("image capturée")
+            else:
+                print("image non capturée")
+                return 1
+            return 0
+        except:
+            return 1
+
 save_image()
 
 
