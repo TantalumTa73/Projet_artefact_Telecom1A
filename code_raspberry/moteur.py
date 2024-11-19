cda997dc4bc783d945c45c55fd16f4f912aee606
martin.devaux <martin.devaux@telecom-paris.fr>
2024-11-19 17:32:57 +0100

    rotation asservie

--- code_raspberry/moteur.py
+++ code_raspberry/moteur.py
@@ -270,5 +270,5 @@ def rota_deg(deg, time_step, temps_accel_decel, side):
 		tick_parc = ticks - acc_tick - dec_tick - asserv_decel[spd]
 		if tick_parc > 0:
 			temps_parc = tick_parc/(100 * spd)
-			rotation_asservi(spd, time_step, temps_parc, temps_accel_decel, temps_accel_decel, "left")
+			rotation_asservi(spd, time_step, temps_parc, temps_accel_decel, temps_accel_decel, side)
 			break
--- code_raspberry/test_moteur.py
+++ code_raspberry/test_moteur.py
@@ -2,4 +2,4 @@ import moteur
 
 
 # Access the argument from the command line
-moteur.rotation_asservi(10, 0.1, 3, 1, 1, "left")
+moteur.rota_deg(360, 0.1, 1, "left")
