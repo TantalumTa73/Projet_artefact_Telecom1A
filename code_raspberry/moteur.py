149bec3acfeb1ef81a0fea408150e42e205471d5
martin.devaux <martin.devaux@telecom-paris.fr>
2024-11-19 09:54:24 +0100

    test

--- code_raspberry/moteur.py
+++ code_raspberry/moteur.py
@@ -39,8 +39,9 @@ def avance_corrige(moteur_princ, ratio, vitesse):
 def rotation_test(temps, vitesse, côté):
     if côté == "left":
         vitesse = -vitesse
+    moteur.set_motor_shutdown_timeout(2)
     moteur.set_motor_speed(vitesse, -vitesse)
-    t.sleep(temps)
     print(moteur.get_encoder_ticks())
+    
 
 
