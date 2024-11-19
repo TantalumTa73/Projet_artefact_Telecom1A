65dd25448f1ab67816a22b6e42f283e9232b9b1e
martin.devaux <martin.devaux@telecom-paris.fr>
2024-11-19 11:27:32 +0100

    runtest

--- code_raspberry/moteur.py
+++ code_raspberry/moteur.py
@@ -66,8 +66,9 @@ def rotation_test(temps, vitesse, côté):
     
 def avance_test():
     print(moteur.get_encoder_ticks())
-    t.sleep(0.5)
+    
     acceleration(30)
+    t.sleep(0.5)
     print(moteur.get_encoder_ticks())
     t.sleep(3)
     print(moteur.get_encoder_ticks())
