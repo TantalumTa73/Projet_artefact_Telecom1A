9f08d457b759769e4134c57463bdb69ab475fccf
Edouard <edouard.vivares@telecom-paris.fr>
2024-11-20 13:52:46 +0100

    Fix

--- code_raspberry/webpage.py
+++ code_raspberry/webpage.py
@@ -254,8 +254,8 @@ def reperage_rotation_prep():
 
 @app.route('/test-aller-drap')
 def aller_drap():
-	print(analyser_drapeau.analyser_drapeau(analyser_drapeau.drapeau_proche(analyse_image.detect_aruco_markers(module_camera.get_image(cam), current_pos))
-))
+	print(analyser_drapeau.analyser_drapeau(analyser_drapeau.drapeau_proche(analyse_image.detect_aruco_markers(module_camera.get_image(cam), current_pos))))
+	return render_template("page.html")	
 
 @app.route('/update')
 def update():
