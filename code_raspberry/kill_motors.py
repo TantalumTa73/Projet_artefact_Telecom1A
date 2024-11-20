c5e760c673ad4bd414f25e9a99c167bb4e5f35ac
jeffrey <jeffrey.tiger@telecom-paris.fr>
2024-11-20 10:42:57 +0100

    creation du fichier kill_motors

---
+++ code_raspberry/kill_motors.py
@@ -0,0 +1,11 @@
+import controller
+import time
+c = controller.Controller()
+print("Shutting down motors...", end="")
+c.set_motor_speed([0,0])
+print("OK")
+
+print("Reseting motors...", end="")
+c.reset()
+print("OK")
+time.sleep(2)
