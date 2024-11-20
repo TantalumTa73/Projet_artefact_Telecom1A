c09d0e077594b3162636b3aa15e19e549535ec84
jeffrey <jeffrey.tiger@telecom-paris.fr>
2024-11-20 13:40:38 +0100

    debug

--- code_raspberry/main.py
+++ code_raspberry/main.py
@@ -53,6 +53,7 @@ def reperage_rotation(cam):
         img, res = module_camera.get_image(cam)
         orientation = ((curr_tick[1]*360)/(2*3.141592*7.85*183.6)-(curr_tick[0]*360)/(2*3.141592*7.85*183.6))
         images.append((img,orientation))
+    moteur.reajustement(curr_tick)
     return images
 
 
